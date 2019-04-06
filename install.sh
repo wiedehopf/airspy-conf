7a95d2f6d62487a3171f9dccb3eef8aaa15af97c
Matthias Wirth <matthias.wirth@gmail.com>
2019-04-06 19:23:04 +0200

    adapt for piaware sd-card 3.7

--- install.sh
+++ install.sh
@@ -29,8 +29,10 @@ systemctl enable airspy_adsb
 
 if [ -f /boot/piaware-config.txt ]
 then
-	#configure piaware to relay mode
+	#configure piaware to custom mode
 	sed -i -e 's@beast - radarcape - relay - other@# added by airspy\n\t\tother {\n\t\t\tlappend receiverOpts "--net-only" "--net-bo-port 30005" "--fix"\n\t\t}\n\n\t\tbeast - radarcape - relay@' /usr/lib/piaware-support/generate-receiver-config
+	#piaware version > 3.7
+	sed -i -e 's@none - other@# added by airspy\n\t\tother {\n\t\t\tlappend receiverOpts "--net-only" "--net-bo-port 30005" "--fix"\n\t\t}\n\n\t\tnone@' /usr/lib/piaware-support/generate-receiver-config
 	piaware-config receiver-type other
 	piaware-config receiver-host localhost
 	piaware-config receiver-port 30005

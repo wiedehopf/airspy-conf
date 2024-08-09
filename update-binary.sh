34bbfb37f0d1d15a8ff9717226dac7a595441829
Matthias Wirth <matthias.wirth@gmail.com>
2024-08-09 07:44:33 +0200

    use bookworm version for arm64 in install script
    fixes: https://github.com/wiedehopf/airspy-conf/issues/6

--- update-binary.sh
+++ update-binary.sh
@@ -4,10 +4,10 @@ set -e
 trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
 
 verlte() {
-    [  "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
+    [[  "$1" == "$(echo -e "$1\n$2" | sort -V | head -n1)" ]]
 }
 verlt() {
-    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+    [[ "$1" == "$2" ]] && return 1 || verlte $1 $2
 }
 
 libc=$(ldd --version | grep -i glibc | grep -o -e '[0-9.]*$')
@@ -34,20 +34,27 @@ fi
 URL="https://github.com/wiedehopf/airspy-conf/raw/master"
 
 OS="buster"
-required_libc="2.28"
+required_buster="2.28"
+required_bookworm="2.36"
 if uname -m | grep -qs armv7; then
     OS="buster"
     ARCH=armv7
     echo "avm7l special case only buster (libc-2.28) and later, found libc version: $libc"
-elif [[ -z "$libc" ]] || verlt "$libc" "$required_libc"; then
+elif uname -m | grep -qs aarch64 && [[ -n "$libc" ]] && ! verlt "$libc" "$required_bookworm"; then
+    OS="bookworm"
+    echo "----------------"
+    echo libc version: "$libc >= $required_bookworm"
+    echo "----------------"
+elif [[ -n "$libc" ]] && ! verlt "$libc" "$required_buster"; then
+    OS="buster"
+    echo "----------------"
+    echo libc version: "$libc >= $required_buster"
+    echo "----------------"
+else
     OS="stretch"
     echo "----------------"
     echo "Seems your system is a bit old, performance may be worse than on buster or newer!"
-    echo "$libc < $required_libc"
-    echo "----------------"
-else
-    echo "----------------"
-    echo "$libc >= $required_libc"
+    echo libc version: "$libc < $required_buster"
     echo "----------------"
 fi
 
